b07f2b3503148631a5817ebc491e84aae0917d2e
Hung ne <hungok2909@gmail.com>
2024-10-15 22:35:40 +0700

    hello Bao an cuc ga

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bao an cut ga, Bao ia ko ra");
-        System.out.println("Hung ia ko ra");
-        System.out.println("Hung ia ko ra");
-        System.out.println("Hung ia ko ra");
-        System.out.println("Hung ia ko ra");
+        System.out.println("Bao an nhieu cuc ga vl!!!");
         System.out.println("Bao eat chicken shit, Bao poops so hard");
     }
 }
