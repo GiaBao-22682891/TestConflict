b21d861ce2230672a6b30389b7a964845b306316
Bao To <you@example.com>
2024-10-15 22:37:44 +0700

    Hung loi lon

---
+++ src/HungLon.java
@@ -0,0 +1,25 @@
+public class HungLon {
+    private String duMa;
+    private int duMe;
+
+    public HungLon(String duMa, int duMe) {
+        this.duMa = duMa;
+        this.duMe = duMe;
+    }
+
+    public String getDuMa() {
+        return duMa;
+    }
+
+    public void setDuMa(String duMa) {
+        this.duMa = duMa;
+    }
+
+    public int getDuMe() {
+        return duMe;
+    }
+
+    public void setDuMe(int duMe) {
+        this.duMe = duMe;
+    }
+}
