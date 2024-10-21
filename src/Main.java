f2887b212320ac2351b23c9d163d006f8ee97794
Bao To <you@example.com>
2024-10-21 23:11:25 +0700

    Hung ga

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bao an cut ga, Bao ia ko ra");
-        System.out.println("Bao an nhieu cuc ga vl!!!");
         System.out.println("Bao eat chicken shit, Bao poops so hard");
+        System.out.println("Hung eat chicken shit, Hung poops so hard ");
+
     }
 }
