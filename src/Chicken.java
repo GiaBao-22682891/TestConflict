de9219606d8801058390179017456fea6f79b3da
Hung ne <hungok2909@gmail.com>
2024-10-21 23:23:02 +0700

    add chicken class

---
+++ src/Chicken.java
@@ -0,0 +1,17 @@
+public class Chicken {
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Chicken{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,9 @@ public class Main {
         System.out.println("Bao eat chicken shit, Bao poops so hard");
         System.out.println("Hung eat chicken shit, Hung poops so hard ");
 
+        Chicken chicken = new Chicken("Bao ga", 21);
+
+        System.out.println(chicken);
+
     }
 }
