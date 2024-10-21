5d02324837aafd58a8c03a7ff272a29daa0532db
Hung ne <hungok2909@gmail.com>
2024-10-21 23:12:24 +0700

    hello world static

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bao an cut ga, Bao ia ko ra");
         System.out.println("Bao an nhieu cuc ga vl!!!");
+        System.out.println(testClass.msg);
         System.out.println("Bao eat chicken shit, Bao poops so hard");
     }
 }
